e8698baad4ca8363f9635616767df44e08c8a340
Anton Matosov <anton.matosov@gmail.com>
2020-03-07 18:31:21 -0800

    safe import of tensorboard

--- Chapter03/03_atari_gan.py
+++ Chapter03/03_atari_gan.py
@@ -6,7 +6,11 @@ import cv2
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from tensorboardX import SummaryWriter
+
+try:
+    from tensorboardX import SummaryWriter
+except:
+    from torch.utils.tensorboard import SummaryWriter
 
 import torchvision.utils as vutils
 
